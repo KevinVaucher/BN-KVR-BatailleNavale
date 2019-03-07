6dabb0cb9bf2c5eaa20a2541b071c67c54e3657f
KevinVaucherCPNV <47849513+KevinVaucher@users.noreply.github.com>
2019-03-07 15:52:48 +0100

    BatailleNavaleSprint1C

---
+++ BatailleNavaleSprint1/cmake-build-debug/CMakeFiles/BatailleNavaleSprint1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BatailleNavaleSprint1.dir/main.c.obj"
+  "BatailleNavaleSprint1.pdb"
+  "BatailleNavaleSprint1.exe"
+  "BatailleNavaleSprint1.exe.manifest"
+  "libBatailleNavaleSprint1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BatailleNavaleSprint1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
